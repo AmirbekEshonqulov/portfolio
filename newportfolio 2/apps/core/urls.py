260cc141d88617bc4faee8bc10a39037c1960979
AmirbekEshonqulov <amirbek8056@gmail.com>
2026-09-25 21:39:51 +0500

    Delete newportfolio 2 directory

--- newportfolio 2/apps/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('set-language/', views.set_language, name='set_language'),
-    path('robots.txt', views.robots_txt, name='robots_txt'),
-]
